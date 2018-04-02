d6e217d495160baacdbe2092ecdb0c0585532ba1
spt30 <seregatoschilin@gmail.com>
2018-04-02 05:37:13 +0500

    мелкие правки

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default class NotePage extends Component {
                 <h1>Авторизуйтесь под github:</h1>
                 <h2>Можно не авторизовываться</h2>
                 <h3>Но лучше авторизоваться</h3>
-                <h4>Ебать, рут <b>/main</b>, а компонент <i>index.js</i></h4>
-                <h5>охуеть</h5>
+                <h4>Рут <b>/main</b>, а компонент <i>index.js</i></h4>
+                <h5>Удивительное дело</h5>
                 <a href="/login">Авторизоваться</a>
             </div>
         );
